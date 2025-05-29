190c524c90354b4fd207d90905a99991ba1c4cd5
LadyGaidouk <212734690+LadyGaidouk@users.noreply.github.com>
2025-05-29 16:32:33 +0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -1,9 +1,9 @@
 <?php include 'header.php'; ?>
     <div class="text">
         <div class="sub-text2">
-            <a href="contact.php" class="clickable"><</a>
+            <a href="contact.php" class="clickable scaleX"><</a>
             <h1>Заказать ли IT проект?</h1>
-            <a href="contact.php" class="clickable"><img src="assets/Picture/>.png" alt="Заказать разработку сайта"></a>
+            <a href="contact.php" class="clickable scaleX">></a>
         </div>
     </div>
     <div class="description">
