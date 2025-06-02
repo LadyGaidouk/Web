d4ae6427aba8e6c944dfdea868a36bbaeb81d1f7
LadyGaidouk <212734690+LadyGaidouk@users.noreply.github.com>
2025-06-02 14:09:20 +0500

    Update 404.php

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
             <h1>
                 Что-то пошло не так.
             </h1>
-            <p style="opacity: 0.5;">Cообщите об ошибке <a href="https://t.me/ITBureau_bot" class="clickable notes">здесь</a> — с нас подарок, за помощь в развитии компании.</p>
+            <p style="opacity: 0.5;">Cообщите об ошибке <a href="https://t.me/ITBureau_bot" class="notes">здесь</a> — с нас подарок, за помощь в развитии компании.</p>
         </div>
     </div>
 </section>
