8ccbd295e91e12fcf616b84041a7121afa49e341
LadyGaidouk <212734690+LadyGaidouk@users.noreply.github.com>
2025-06-03 15:28:18 +0500

    Update thanku.php

--- thanku.php
+++ thanku.php
@@ -1,11 +1,9 @@
 <?php include 'header.php'; ?>
     <div class="text thanks">
         <h1>Благодарим за доверие, вернемся с обратной связью.</h1>
-        <p style="opacity: 0.5;">Через 2 секунды запустится игра. Задача: собрать двоичный код.</p>
-        <p style="opacity: 0.5;">Предлагаем между делом заглянуть в <a href="https://t.me/notes_ITBureau" class="notes clickable">заметки</a> от IT Bureau.</p>
+        <p style="opacity: 0.5;"> Оставили для Вас небольшую игру, чтоб отдохнуть. Задача: собирать двоичный код.</p>
         <canvas id="gameCanvas"></canvas>
-        <div id="word-progress">Слово: _________</div>
-        <div id="score">Очки: 0</div>
+        <div id="score">0</div>
         <div class="game-over" id="gameOver">
         </div>
     </div>
